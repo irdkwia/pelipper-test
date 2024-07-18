irdkwia/pelipper-test

commit ca7c0121e75075b0d3e60a89628e9842cf0ee664

Minor Discord fixes (#4)

handlers/httphandler.py
@@ -245,7 +245,7 @@ class CustomHandler(BaseHTTPRequestHandler):
                         print(rescued_identifier)
                         if ty == ProfileType.DISCORD and prf.scode:
                             rescued_user_name = rescued_identifier
-                        asyncio.run_coroutine_threadsafe(discord_bot.notify_rescue(rescued_user_name, rq.team, rq.title, rq.message, rq.dungeon, rq.floor), discord_bot.bot.loop)
+                        asyncio.run_coroutine_threadsafe(discord_bot.notify_rescue(rescued_user_name, rq.team, rq.title, rq.message, rq.dungeon, rq.floor, format_rescue_code(rq.rid)), discord_bot.bot.loop)
 
                 elif new_path=="/rescue/rescueComplete.asp":
                     rq = db.get_elements(RescueRequest, {"rid": select}, limit=1)
@@ -281,7 +281,7 @@ class CustomHandler(BaseHTTPRequestHandler):
                                     if rescuer_ty == ProfileType.DISCORD and prf.scode:
                                         rescuer_user_name = rescuer_identifier
 
-                                    asyncio.run_coroutine_threadsafe(discord_bot.send_aok(rescued_user_name, rescuer_user_name, rq.team, aok.team, aok.title, aok.message, rq.dungeon, rq.floor), discord_bot.bot.loop)
+                                    asyncio.run_coroutine_threadsafe(discord_bot.send_aok(rescued_user_name, rescuer_user_name, rq.team, aok.team, aok.title, aok.message, rq.dungeon, rq.floor, format_rescue_code(rq.rid)), discord_bot.bot.loop)
                         else:
                             buffer += b'\x00\x00\x00\x00'
                     else:
@@ -622,13 +622,10 @@ class CustomHandler(BaseHTTPRequestHandler):
             if not message:
                 message = "We were defeated! Please help!"
 
-            rid_str = f"{rq.rid:012d}"
-            code = f"{rid_str[:4]}-{rid_str[4:8]}-{rid_str[8:]}"
-
             vars = {
                 "title": title,
                 "dungeon": base_game_constants.format_floor(rq.dungeon, rq.floor),
-                "code": code,
+                "code": format_rescue_code(rq.rid),
                 "team": rq.team,
                 "game": game,
                 "message": message
@@ -668,3 +665,7 @@ class ProfileType(Enum):
             return (ProfileType.EMAIL, combined_name)
         else:
             return (ProfileType.UNKNOWN, combined_name)
+
+def format_rescue_code(rid):
+    rid_str = f"{rid:012d}"
+    return f"{rid_str[:4]}-{rid_str[4:8]}-{rid_str[8:]}"

discord_bot.py
@@ -17,7 +17,7 @@ async def send_signup_code(username_or_id: str, signup_code: str):
     else:
         raise Exception("Failed to find user: " + username_or_id)
 
-async def notify_rescue(username_or_id: Optional[str], team: str, title: str, message: str, dungeon_id: int, floor: int):
+async def notify_rescue(username_or_id: Optional[str], team: str, title: str, message: str, dungeon_id: int, floor: int, code: str):
     if not constants.DISCORD_UPDATE_CHANNEL_ID:
         return
 
@@ -29,11 +29,12 @@ async def notify_rescue(username_or_id: Optional[str], team: str, title: str, me
 
     description = f"Team {team} requested a rescue!"
     if user:
-        description = f"<@!{user.id}> requested a rescue!" # <@!{user_id}> mentions without pinging the user
+        description = f"<@{user.id}> requested a rescue!"
 
     embed = discord.Embed(title=f"New SOS Mail", description=description, color=0xff0000)
     embed.add_field(name="Team Name", value=team, inline=False)
     embed.add_field(name="Dungeon", value=base_game_constants.format_floor(dungeon_id, floor), inline=False)
+    embed.add_field(name="Rescue Number", value=code, inline=False)
     if title:
         embed.add_field(name="Title", value=title, inline=False)
     if message:
@@ -43,10 +44,12 @@ async def notify_rescue(username_or_id: Optional[str], team: str, title: str, me
     except Exception as error:
         print("Error:", error)
 
-async def send_aok(rescued_username_or_id: Optional[str], rescuer_username_or_id: Optional[str], rescued_team: str, rescuer_team: str, title: str, message: str, dungeon_id: int, floor: int):
+async def send_aok(rescued_username_or_id: Optional[str], rescuer_username_or_id: Optional[str], rescued_team: str, rescuer_team: str, title: str, message: str, dungeon_id: int, floor: int, code: str):
     def apply_embed_fields(embed: discord.Embed):
-        embed.add_field(name="Team Name", value=rescuer_team, inline=False)
-        embed.add_field(name="Dungeon", value=base_game_constants.format_floor(dungeon_id, floor), inline=False)
+        embed.add_field(name="Rescued Team", value=rescued_team)
+        embed.add_field(name="Rescuer", value=rescuer_team)
+        embed.add_field(name="Dungeon", value=base_game_constants.format_floor(dungeon_id, floor))
+        embed.add_field(name="Rescue Number", value=code)
         if title:
             embed.add_field(name="Title", value=title, inline=False)
         if message:
@@ -60,10 +63,10 @@ async def send_aok(rescued_username_or_id: Optional[str], rescuer_username_or_id
 
         description_rescuer = f"Team {rescuer_team}"
         if rescuer_user:
-            description_rescuer = f"<@!{rescuer_user.id}>"
+            description_rescuer = f"<@{rescuer_user.id}>"
         description_rescued = f"Team {rescued_team}"
         if rescued_user:
-            description_rescued = f"<@!{rescued_user.id}>"
+            description_rescued = f"<@{rescued_user.id}>"
         description = f"{description_rescuer} has rescued {description_rescued}!"
         embed = discord.Embed(title="New A-OK Mail", description=description, color=0x00ff00)
 

loadtest.sh
@@ -1,0 +1,13 @@
+#!/bin/bash
+URL="https://wfc.skytemple.org"
+
+CONCURRENT_REQUESTS=50
+
+while true; do
+    echo "$(date): Sending $CONCURRENT_REQUESTS concurrent requests to $URL"
+
+    urls=$(printf "$URL\n%.0s" $(seq 1 $CONCURRENT_REQUESTS))
+
+    echo "$urls" | xargs -n1 -P$CONCURRENT_REQUESTS -I {} curl -o /dev/null -s -w "%{http_code} " {} | tr '\n' ' '
+    echo
+done

commit 9ed9fe12b14cb17b95fb71857c4cbefc8f070a6e

Fix crash when sending A-OK mails via Discord bot
Fixes a regression from https://github.com/irdkwia/pelipper-test/pull/4 that caused sending A-OK mails to fail.

handlers/httphandler.py
@@ -273,10 +273,12 @@ class CustomHandler(BaseHTTPRequestHandler):
                                 if len(rescued_prf)>0:
                                     rescued_prf = rescued_prf[0]
 
+                                    rescued_user_name = None
                                     (rescued_ty, rescued_identifier) = ProfileType.into_parts(rescued_prf.email)
                                     if rescued_ty == ProfileType.DISCORD and rescued_prf.scode:
                                         rescued_user_name = rescued_identifier
 
+                                    rescuer_user_name = None
                                     (rescuer_ty, rescuer_identifier) = ProfileType.into_parts(prf.email)
                                     if rescuer_ty == ProfileType.DISCORD and prf.scode:
                                         rescuer_user_name = rescuer_identifier